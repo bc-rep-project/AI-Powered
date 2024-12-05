e295dc01c8543848d476a47724919df8fdf1ab97
bc-rep-project <johannderbiel@gmail.com>
2024-12-05 08:07:55 +0200

    Update PORT configuration in railway.toml and start.py for application deployment
    - Changed the PORT environment variable from 8000 to 8080 in railway.toml to align with deployment requirements.
    - Updated start.py to reflect the new PORT value, ensuring the application runs on the correct port during local execution.
    
    These changes improve the application's configuration for deployment and local testing, facilitating a smoother development experience.

--- start.py
+++ start.py
@@ -4,5 +4,5 @@ import uvicorn
 
 if __name__ == "__main__":
     load_dotenv()
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False) 
