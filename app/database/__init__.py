56a7395f93428f725687613d40114907d0462f7f
bc-rep-project <johannderbiel@gmail.com>
2024-12-27 03:13:22 +0200

    feat: add database session management and enhance PostgreSQL integration
    - Introduced a new `get_db` function in `postgresql.py` for managing database sessions, improving resource handling.
    - Updated `__init__.py` to include `get_db` in the module's exports, ensuring accessibility throughout the application.
    - Enhanced the overall database interaction capabilities, laying the groundwork for more robust data handling in the FastAPI application.
    
    These changes improve the efficiency and reliability of database operations, contributing to a more maintainable backend structure.

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,5 +1,5 @@
 # This file makes the directory a Python package 
-from .postgresql import test_database_connection, engine, Base
+from .postgresql import test_database_connection, engine, Base, get_db
 from .mongodb import mongodb
 
-__all__ = ['test_database_connection', 'engine', 'Base', 'mongodb'] 
+__all__ = ['test_database_connection', 'engine', 'Base', 'mongodb', 'get_db'] 
--- app/database/postgresql.py
+++ app/database/postgresql.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 from sqlalchemy import create_engine, Column, Integer, String, text
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
 
 class Settings(BaseSettings):
     DB_USER: str
@@ -18,9 +19,18 @@ settings = Settings()
 DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create declarative base
 Base = declarative_base()
+
+def get_db():
+    """Get database session."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def test_database_connection():
     """Test the database connection by executing a simple query"""
